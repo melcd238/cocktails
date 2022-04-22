030e8f30ce5b98298e15b461347ed045658843d2
melcd238 <melcd238@gmail.com>
2022-04-22 10:16:24 +0200

    setting-up the router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
 // import pages
 import Home from './Pages/Home'
 import About from './Pages/About'
@@ -12,9 +12,17 @@ import Navbar from './Components/Navbar'
 
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+   <Router>
+     <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>} />
+        <Route path='singleCocktail/:id' element={<SingleCocktail/>}/>
+        <Route path='*' element={<Error/>} />
+      </Routes>
+   </Router>
+     
+  
   )
 }
 
