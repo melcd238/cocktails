dd5595ef01a3e6d74b69d0961f477cc27f131b23
melcd238 <melcd238@gmail.com>
2022-04-23 18:40:31 +0200

    setting up About page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <main>
-      <h2>about page</h2>
+    <main className='aboutMain'>
+      <div className='cardAbout'>
+         <h2 className='titleAbout'>About Cocktails</h2>
+         <p className='pAbout'>
+         This application was created with REACT and thanks to the API: <br/>
+         <a href='https://www.thecocktaildb.com/api.php'>TheCocktailDB.com.</a><br/>
+         Thanks to the site <a href='https://www.pexels.com/fr-fr/'>Pexels</a> and to Arina Krasnikova for her photo.<br/>
+         You can find the source code on github: 
+         </p>
+      </div>
+      
     </main>
   )
 }
