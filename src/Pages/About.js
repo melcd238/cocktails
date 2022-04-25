27696e983382ecce5bb8b5cf10d2ad2ae4bb870c
melcd238 <melcd238@gmail.com>
2022-04-25 19:04:23 +0200

    setting up about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
          This application was created with REACT and thanks to the API: <br/>
          <a href='https://www.thecocktaildb.com/api.php'>TheCocktailDB.com</a><br/>
          Thanks to the site <a href='https://www.pexels.com/fr-fr/'>Pexels</a>  for the background photo.
-         You can find the source code on github: 
+         You can find the source code on github: <a href='https://github.com/melcd238/cocktails'>github cokctails</a>
          </p>
       </div>
       
